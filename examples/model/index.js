9f9a3327f34a4db8e4121c87b98d8da528d33da2
Bonezegei <jofelbatutay@gmail.com>
2026-06-25 00:56:01 +0800

    update

---
+++ examples/model/index.js
@@ -0,0 +1,12 @@
+import Bonezegei from './../../src/Bonezegei.js';
+
+const bz = new Bonezegei();
+
+export function App() {
+  const render = bz.createRenderer();
+  const state = bz.reactive(
+    { username: '' },
+    () => render('#app', state)
+  );
+  render('#app', state);
+}
