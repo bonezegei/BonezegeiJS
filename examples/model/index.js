1d7f7c1b34e7d47eadf909c51adcd5743d88dbc3
Bonezegei <jofelbatutay@gmail.com>
2026-06-25 00:58:18 +0800

    update

--- examples/model/index.js
+++ examples/model/index.js
@@ -3,10 +3,12 @@ import Bonezegei from './../../src/Bonezegei.js';
 const bz = new Bonezegei();
 
 export function App() {
+
   const render = bz.createRenderer();
   const state = bz.reactive(
     { username: '' },
     () => render('#app', state)
   );
   render('#app', state);
+  
 }
